88f63cbcd6661f05a3eb248b19e0b17e88d326da
danisherror <danishmsrit780@gmail.com>
2023-09-21 23:16:21 +0530

    7th

--- client/src/artist/Signin.js
+++ client/src/artist/Signin.js
@@ -1,82 +1,80 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import React,{useState} from 'react';
+import { useHistory } from 'react-router-dom';
+import '../styles/external.css';
 
-const SigninArtist = () => {
-  const history = useHistory("");
 
-  const [inpval, setINP] = useState({
-    email: "",
-    password: ""
+const Signinuser = () => {
+    const history = useHistory("");
 
-  })
-
-  const setdata = (e) => {
-    console.log(e.target.value);
-    const { name, value } = e.target;
-    setINP((preval) => {
-      return {
-        ...preval,
-        [name]: value
-      }
+    const [inpval, setINP] = useState({
+        email: "",
+        password: ""
+        
     })
-  }
-  const addinpdata = async (e) => {
-    e.preventDefault();
 
-    const { email, password } = inpval;
-
-    const res = await fetch("/api/artist/signin", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        email, password
-      })
-    });
-
-    const data = await res.json();
-    if (res.status === 401 || !data) {
-      console.log("error ");
-      alert("error user not found");
-
-    } else {
-      alert("Signin");
-      console.log("data added");
-      history.push(`/artisthome/${data.token}`);
+    const setdata = (e) => {
+        console.log(e.target.value);
+        const { name, value } = e.target;
+        setINP((preval) => {
+            return {
+                ...preval,
+                [name]: value
+            }
+        })
     }
-  }
+    const addinpdata = async (e) => {
+        e.preventDefault();
+
+        const { email, password } = inpval;
+
+        const res = await fetch("/api/artist/signin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email, password
+            })
+        });
+
+        const data = await res.json();
+        if (res.status === 401 || !data) {
+            console.log("error ");
+            alert("error user not found");
+
+        } else {
+            alert("Signin");
+            console.log("data added");
+            history.push(`/artisthome/${data.token}`);
+        }
+    }
   return (
+<div class="fullscreen-div">
+        <div>
+            <h2>
+              Your Work Counts!Enter Login details to list your products.
+            </h2>
+        </div>
+        <form class="form-signin">
+            <div class="form-group">
+                <h2>Login</h2>
+            </div>
 
-    <div id="container-main">
-      <div class="login-box">
-
-        <h2>Login</h2>
-
-        <form>
-          <div class="user-box">
-            <input type="email" value={inpval.email} onChange={setdata} name="email" required="" />
-            <label>Email</label>
-          </div>
-          <br />
-          <div class="user-box">
-            <input type="password" value={inpval.password} onChange={setdata} name="password" required="" />
-            <label>Password</label>
-          </div>
-          <button class="btn-sub" type="submit" onClick={addinpdata}>
-            <a>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              Submit
-            </a>
-          </button>
+            <div class="form-group">
+                <input type="email" class="form-control" value={inpval.email} onChange={setdata} name="email" required="" />
+                <label>Email</label>
+            </div>
+            <br/>
+            <div class="form-group">
+                <input type="password" class="form-control" value={inpval.password} onChange={setdata} name="password" required="" />
+                <label>Password</label>
+            </div>
+            <button class="btn btn-primary" type="submit" onClick={addinpdata}>
+            Submit
+            </button>
         </form>
-      </div>
     </div>
-
   )
 }
 
-export default SigninArtist;
+export default Signinuser;
